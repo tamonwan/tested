56c8f4fe6c5316810c9901cf5f3db2bf3a6cf1df
Pop <pop@Tamonwans-MacBook-Pro.local>
2017-04-06 13:45:21 +0700

    jaaaaaa

--- testdb/ViewController.swift
+++ testdb/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
@@ -15,11 +17,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var nameF: UITextField!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        Alamofire.request(.POST,"https://localhost/topost",parameters: ["foo":nameF.text])
+            .response { request, response, data, error in
+                    print(request)
+                    print(response)
+                    print(error)
+        }
+        
+    }
 
 }
 
